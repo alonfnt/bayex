08e35372f1a83585046da6059f1a713f7ca0d65a
Albert Alonso <alonfnt@pm.me>
2021-09-06 10:54:58 +0200

    fix(optim): fix nan issue + some docstrings

--- bayex/optim.py
+++ bayex/optim.py
@@ -4,25 +4,59 @@ import jax.numpy as jnp
 from jax import jacrev, jit, lax, ops, partial, random, vmap
 from jax._src.prng import PRNGKeyArray
 from jax.scipy.stats import norm
-from jaxlib.xla_extension import DeviceArray
 
 from .gp import predict, train
 
 
-def jacobian(f):
+def jacobian(f: Callable) -> Callable:
     return jit(jacrev(f))
 
 
-def expected_improvement(x_pred, A, x, y, y_max, xi=0.01):
+def expected_improvement(
+    x_pred: jnp.ndarray,
+    A: jnp.ndarray,
+    x: jnp.ndarray,
+    y: jnp.ndarray,
+    xi: float = 0.01,
+):
     """
     Computes the expected improvement at points x_pred over a
-    gaussian process trained on x and y.
+    Gaussian process trained on x and y.
+
+    Parameters:
+    -----------
+    x_pred: The points at which the improvement is computed.
+    A: Trained hyperparameter of the GP.
+    x: Sampled points.
+    y: Sampled targets.
+    xi: Parameter to balance exploration-exploitation.
+
+    Returns:
+    --------
+    ei: The expected improvement of x_pred on the trained GP.
     """
+    y_max = y.max()
     mu, std = predict(A, x, y, xtest=x_pred)
     imp = mu.T - y_max - xi
     z = imp / std
     ei = imp * norm.cdf(z) + std * norm.pdf(z)
     return ei
+
+
+def replace_nan_values(arr: jnp.ndarray) -> jnp.ndarray:
+    """
+    Replaces the NaN values (if any) in arr with 0.
+
+    Parameters:
+    -----------
+    arr: The array where NaN is removed from.
+
+    Returns:
+    --------
+    The array with all the NaN elements replaced with 0.
+    """
+    # todo(alonfnt): Find a more robust solution.
+    return jnp.where(jnp.isnan(arr), 0, arr)
 
 
 @jit
@@ -32,11 +66,10 @@ def suggest_next(
     x: jnp.ndarray,
     y: jnp.ndarray,
     bounds: jnp.ndarray,
-    y_max: float,
-    n_seed: int = 10,
+    n_seed: int = 1000,
     lr: float = 0.1,
     n_epochs: int = 150,
-) -> Tuple[DeviceArray, PRNGKeyArray]:
+) -> Tuple[jnp.ndarray, PRNGKeyArray]:
     """
     Suggests the new point to sample by optimizing the acquisition function.
 
@@ -49,11 +82,15 @@ def suggest_next(
             variables.y_max: The current maximum value of the target values Y.
     n_seed (optional): the number of points to probe and minimize until
             finding the one that maximizes the acquisition functions.
+    lr (optional): The step size of the gradient descent.
+    n_epochs (optional): The number of steps done on the descent to minimize
+            the seeds.
 
 
     Returns:
     --------
-
+    A tuple with the parameters that maximize the acquisition function and a
+    jax PRGKey to be used in the next sampling.
     """
 
     key1, key2 = random.split(key, 2)
@@ -63,41 +100,49 @@ def suggest_next(
         key1, shape=(n_seed, dim), minval=bounds[:, 0], maxval=bounds[:, 1]
     )
 
-    acq = partial(expected_improvement, A=A, x=x, y=y, y_max=y_max)
+    acq = partial(expected_improvement, A=A, x=x, y=y)
 
-    def minObj(x):
-        return -acq(x.reshape(-1, dim)).reshape()
-
-    J = jacobian(minObj)
-    HS = vmap(lambda x: x - lr * J(x))
+    J = jacobian(lambda x: acq(x.reshape(-1, dim)).reshape())
+    HS = vmap(lambda x: x + lr * J(x))
 
     domain = lax.fori_loop(0, n_epochs, lambda _, d: HS(d), domain)
-
-    clipped_domain = jnp.clip(
-        domain.flatten(), a_min=bounds[:, 0], a_max=bounds[:, 1]
+    domain = jnp.clip(
+        domain.reshape(-1, dim), a_min=bounds[:, 0], a_max=bounds[:, 1]
     )
-    ys = acq(clipped_domain)
-    x_max = clipped_domain[ys.argmax()]
-    return x_max, key2
+    domain = replace_nan_values(domain)
 
-
-def expand(arr, new_size, axis=0):
-    shape = arr.shape
-    new_shape = ops.index_update(shape, axis, new_size)
-    addition = jnp.empty(new_shape)
-    new_arr = jnp.concatenate((arr, addition), axis=axis)
+    ys = acq(domain)
+    next_X = domain[ys.argmax()]
+    return next_X, key2
 
 
 def optim(
     f: Callable,
     constrains: Dict,
-    seed: int,
+    seed: int = 42,
     n_init: int = 5,
     n: int = 10,
     xi: float = 0.01,
 ):
+    """
+    Finds the inputs of 'f' that yield the maximum value between the given
+    'constrains', after 'n_init' + 'n' iterations.
 
-    assert n > 0, "num of iterations n should be a positive integer"
+    Parameters:
+    -----------
+    f: Function to optimize.
+    constrains: Dictionary with the domain of each input variable.
+    seed: Pseudo-random number generator seed for reproducibility
+    n_init: Number of initial evaluations before suggesting optimized samples.
+    n: Number of sampling iterations.
+    xi: Parameter to balance exploration-exploitation.
+
+    Returns:
+    --------
+    The parameters that maximize the given 'f'.
+    """
+
+    assert n > 0, "Num of iterations n should be a positive integer"
 
     key = random.PRNGKey(seed)
     dim = len(constrains)
@@ -111,26 +156,24 @@ def optim(
     Y = vmap(f)(*X.T)
 
     # Create an empty array with the same values to not perjudicate the gp
-    X = jnp.vstack((X, jnp.empty((n, dim))))
-    X = ops.index_update(X, ops.index[n_init:], X[n_init - 1])
-    Y = jnp.hstack((Y, jnp.empty((n))))
-    Y = ops.index_update(Y, ops.index[n_init:], Y[n_init - 1])
+    X = jnp.pad(X, ((0, n), (0, 0)), mode="edge")
+    Y = jnp.pad(Y, ((0, n)), mode="edge")
 
     A, p, s = jnp.zeros((1, 1)), 0, 1
 
+    # todo(alonfnt): make it JAX friendly
     for i in range(n):
         idx = n_init + i
         A, p, s = train(A, p=p, scale=s, x=X, y=Y, nsteps=5)
-        xmax, key = suggest_next(
+        max_params, key = suggest_next(
             key,
             A,
             X,
             Y,
             bounds,
-            y_max=Y.max(),
         )
-        print(f"New Max point: {xmax} and {f(xmax)}")
-        X = ops.index_update(X, ops.index[idx, ...], xmax)
-        Y = ops.index_update(Y, ops.index[idx], f(xmax))
+        print(f"New max point: {max_params} and {f(*max_params)}")
+        X = ops.index_update(X, ops.index[idx, ...], max_params)
+        Y = ops.index_update(Y, ops.index[idx], f(*max_params))
     xmax = X[Y.argmax()]
     return xmax
