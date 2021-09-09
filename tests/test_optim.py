e0dc65927f20febe18e14212445ab8dfe1e9ed9f
Albert Alonso <alonfnt@pm.me>
2021-09-09 10:51:18 +0200

    tests: changed target_funcs from lambda to def

--- tests/test_optim.py
+++ tests/test_optim.py
@@ -1,17 +1,19 @@
 import jax
-import bayex
 import jax.numpy as jnp
 
+import bayex
 
 KEY = jax.random.PRNGKey(42)
 SEED = 42
+
 
 def test_1D_optim():
     """
     Complete test of the optim for a very basic 1d function.
     """
 
-    f = lambda x: jnp.sin(x) - x / (jnp.cos(x) + 1) + x ** 3 - (x + 1) ** 4
+    def f(x):
+        return jnp.sin(x) - x / (jnp.cos(x) + 1) + x ** 3 - (x + 1) ** 4
 
     TARGET = -0.3747
     bounds = dict(x=(-2, 2))
@@ -20,12 +22,14 @@ def test_1D_optim():
     target_found = f(param)
     assert jnp.allclose(TARGET, target_found, atol=1e-02)
 
+
 def test_2D_optim():
     """
     Complete test of the optim function for 2 dimensional function.
     """
-    
-    f = lambda x, y: -y ** 2 - (x - y) ** 2 + 3 * x / y - 2
+
+    def f(x, y):
+        return -(y ** 2) - (x - y) ** 2 + 3 * x / y - 2
 
     TARGET = 2.24936
     bounds = dict(x=(0, 5), y=(1, 4))
