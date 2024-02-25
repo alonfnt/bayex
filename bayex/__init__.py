083665d24551c56357a2bd18c07b87b8291cdf63
Albert Alonso <albert.alonso@nbi.ku.dk>
2024-02-25 12:39:10 +0000

    build: update version to 0.2.0

--- bayex/__init__.py
+++ bayex/__init__.py
@@ -2,9 +2,10 @@ from bayex.src.optimizer import Optimizer, OptimizerState
 from bayex.src import domain
 
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
         "Optimizer",
         "OptimizerState",
+        "domain",
 ]
